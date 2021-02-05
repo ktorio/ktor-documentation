ade2135278b928cf6781bc0e8f486c4dbd98c744
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-02-05 14:51:24 +0300

    KTOR-2026 cosmetics

---
+++ codeSnippets/snippets/_misc_client/GetStartedClient.kt
@@ -0,0 +1,10 @@
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+
+suspend fun main() {
+    val client = HttpClient(CIO)
+    val response: HttpResponse = client.get("https://ktor.io/")
+    println(response.status)
+}
