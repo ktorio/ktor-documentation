1f205e748c31aa0a3dee193ffd0374696498ada0
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2021-04-21 20:33:23 +0300

    KTOR-2559 Test building docker image, running container and checking response from the server

--- codeSnippets/snippets/e2e/src/main/kotlin/e2e/gradle.kt
+++ codeSnippets/snippets/e2e/src/main/kotlin/e2e/gradle.kt
@@ -2,9 +2,15 @@ package e2e
 
 import java.io.InputStream
 
-fun runGradleApp(): Process {
+fun runGradleApp(): Process = runGradle("run")
+
+fun runGradle(vararg args: String): Process {
     val gradlewPath = System.getProperty("gradlew") ?: error("System property 'gradlew' should point to Gradle Wrapper file")
-    return ProcessBuilder(gradlewPath, "-Dorg.gradle.logging.level=quiet", "--quiet", "run").start()
+    val processArgs = listOf(gradlewPath, "-Dorg.gradle.logging.level=quiet", "--quiet") + args
+
+    val process = ProcessBuilder(processArgs).start()
+    process.waitFor()
+    return process
 }
 
 fun InputStream.readString(): String = readAllBytes().toString(Charsets.UTF_8)
