ccbd849a28ce7e69f56dac76eaa595d814e93b91
Rustam <rxsinukov@gmail.com>
2021-05-18 15:06:11 +0200

    Update UploadProgress.kt

--- codeSnippets/snippets/_misc_client/UploadProgress.kt
+++ codeSnippets/snippets/_misc_client/UploadProgress.kt
@@ -3,5 +3,6 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 
 val response: HttpResponse = client.post("http://localhost:8080/post") {
-    body = observableBodyOf(content) { bytesSendTotal: Long, contentLength: Long -> TODO("update UI") }
+    body = content
+    onDownload { bytesSendTotal: Long, contentLength: Long -> TODO("update UI") }
 }
