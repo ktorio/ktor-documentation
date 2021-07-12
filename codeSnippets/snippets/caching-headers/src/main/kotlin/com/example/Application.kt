87c1e2632b53c83bf050c31f16a69988a7e8775c
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-07-12 16:37:06 +0300

    KTOR-1873 add caching headers example

---
+++ codeSnippets/snippets/caching-headers/src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,51 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.html.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import kotlinx.css.*
+import kotlinx.html.*
+
+fun Application.main() {
+    install(CachingHeaders) {
+        options { outgoingContent ->
+            when (outgoingContent.contentType?.withoutParameters()) {
+                ContentType.Text.CSS -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 3600))
+                else -> null
+            }
+        }
+    }
+    routing {
+        get("/html-dsl") {
+            call.respondHtml {
+                head {
+                    link(rel = "stylesheet", href = "/styles.css", type = "text/css")
+                }
+                body {
+                    h1(classes = "page-title") {
+                        +"Hello from Ktor!"
+                    }
+                }
+            }
+        }
+        get("/styles.css") {
+            call.respondCss {
+                body {
+                    backgroundColor = Color.darkBlue
+                    margin(0.px)
+                }
+                rule("h1.page-title") {
+                    color = Color.white
+                }
+            }
+        }
+    }
+}
+
+suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
