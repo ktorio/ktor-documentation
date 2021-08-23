8e91fe6f60801b3efa173bc243c8abc94d7b91c2
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-08-23 17:00:09 +0300

    KTOR-2995 server example

---
+++ codeSnippets/snippets/server-websockets/README.md
@@ -0,0 +1,10 @@
+# Server WebSockets
+
+A sample Ktor project showing how to use [WebSockets](https://ktor.io/docs/websocket.html) in a Ktor server.
+
+## Running
+
+To run a sample, execute the following command in a repository's root directory:
+```bash
+./gradlew :server-websockets:run
+```
---
+++ codeSnippets/snippets/server-websockets/src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,67 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.http.cio.websocket.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.websocket.*
+import java.time.*
+import java.util.*
+import java.util.concurrent.atomic.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module(testing: Boolean = false) {
+    install(WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+
+        webSocket("/echo") {
+            for (frame in incoming) {
+                when (frame) {
+                    is Frame.Text -> {
+                        val receivedText = frame.readText()
+                        outgoing.send(Frame.Text("Hi, $receivedText!"))
+                        if (receivedText.equals("bye", ignoreCase = true)) {
+                            close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+                        }
+                    }
+                }
+            }
+        }
+
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
+        webSocket("/chat") {
+            println("Adding user!")
+            val thisConnection = Connection(this)
+            connections += thisConnection
+
+            for (frame in incoming) {
+                when (frame) {
+                    is Frame.Text -> {
+                        val receivedText = frame.readText()
+                        val textWithUsername = "[${thisConnection.name}]: $receivedText"
+                        connections.forEach {
+                            it.session.send(textWithUsername)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+class Connection(val session: DefaultWebSocketSession) {
+    companion object {
+        var lastId = AtomicInteger(0)
+    }
+
+    val name = "user${lastId.getAndIncrement()}"
+}
