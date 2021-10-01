0d2a5f9faf897a8f95fb3bf5c2830862486cef2e
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-10-01 10:21:37 +0300

    rename CSSBuilder

--- codeSnippets/snippets/conditional-headers/src/main/kotlin/com/example/Application.kt
+++ codeSnippets/snippets/conditional-headers/src/main/kotlin/com/example/Application.kt
@@ -52,6 +52,6 @@ fun Application.main() {
     }
 }
 
-suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
-    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+suspend inline fun ApplicationCall.respondCss(builder: CssBuilder.() -> Unit) {
+    this.respondText(CssBuilder().apply(builder).toString(), ContentType.Text.CSS)
 }
