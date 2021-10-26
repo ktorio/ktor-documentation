9954b02048d4769c894bacdd5c21959531cdad70
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-10-26 18:26:08 +0300

    KTOR-1804 call-id sample

---
+++ codeSnippets/snippets/call-id/README.md
@@ -0,0 +1,15 @@
+# CallId
+
+A sample Ktor project showing how to use the [CallId](https://ktor.io/docs/call-id.html) plugin to trace requests.
+
+## Running
+
+To run a sample, execute the following command in a repository's root directory:
+```bash
+./gradlew :call-id:run
+```
+Then, open the [request.http](request.http) file and make the `GET` request with a randomly generated `X-Request-ID` header value. The application output will show the request ID in the log:
+
+```
+2021-10-26 18:22:55.164 [eventLoopGroupProxy-4-1] 13a3feb9-d83b-40ad-839a-d7d5cb147f54 INFO  Application - 200 OK: GET - /
+```
---
+++ codeSnippets/snippets/call-id/src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,24 @@
+package com.example
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module(testing: Boolean = false) {
+    install(CallId) {
+        verify("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=-")
+        header(HttpHeaders.XRequestId)
+    }
+    install(CallLogging) {
+        callIdMdc("call-id")
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+    }
+}
