75178242d1bdbf1e74a1f2b925ab8becebca07bb
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-11-29 12:43:58 +0300

    KTOR-3486 testing websockets

--- codeSnippets/snippets/server-websockets/src/test/kotlin/com/example/ModuleTest.kt
+++ codeSnippets/snippets/server-websockets/src/test/kotlin/com/example/ModuleTest.kt
@@ -1,13 +1,32 @@
 package com.example
 
+import io.ktor.client.plugins.websocket.*
 import io.ktor.server.application.*
-import io.ktor.http.cio.websocket.*
+import io.ktor.websocket.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 
 class ModuleTest {
     @Test
     fun testConversation() {
+        testApplication {
+            val client = createClient {
+                install(WebSockets)
+            }
+
+            client.webSocket("/echo") {
+                val greetingText = (incoming.receive() as? Frame.Text)?.readText() ?: ""
+                assertEquals("Please enter your name", greetingText)
+
+                send(Frame.Text("JetBrains"))
+                val responseText = (incoming.receive() as Frame.Text).readText()
+                assertEquals("Hi, JetBrains!", responseText)
+            }
+        }
+    }
+
+    @Test
+    fun testConversationLegacyApi() {
         withTestApplication(Application::module) {
             handleWebSocketConversation("/echo") { incoming, outgoing ->
                 val greetingText = (incoming.receive() as Frame.Text).readText()
@@ -16,10 +35,6 @@ class ModuleTest {
                 outgoing.send(Frame.Text("JetBrains"))
                 val responseText = (incoming.receive() as Frame.Text).readText()
                 assertEquals("Hi, JetBrains!", responseText)
-
-                outgoing.send(Frame.Text("bye"))
-                val closeReason = (incoming.receive() as Frame.Close).readReason()?.message
-                assertEquals("Client said BYE", closeReason)
             }
         }
     }
