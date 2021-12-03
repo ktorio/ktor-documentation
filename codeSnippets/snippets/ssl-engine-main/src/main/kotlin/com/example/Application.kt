cb7273083dee0b46ec4cc08450bc8d015713313c
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-12-03 16:35:40 +0300

    KTOR-3428 add HttpsRedirect sample

--- codeSnippets/snippets/ssl-engine-main/src/main/kotlin/com/example/Application.kt
+++ codeSnippets/snippets/ssl-engine-main/src/main/kotlin/com/example/Application.kt
@@ -1,12 +1,18 @@
 package com.example
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
+    install(HttpsRedirect) {
+        sslPort = 8443
+        permanentRedirect = true
+    }
+    install(XForwardedHeaderSupport)
     routing {
         get("/") {
             call.respondText("Hello, world!")
