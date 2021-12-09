86fad93aa258013e6ff85abe8b210d6d5a4ce735
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-12-09 15:53:04 +0300

    KTOR-3429 fix test

--- codeSnippets/snippets/tutorial-website-interactive/src/test/kotlin/com/example/ApplicationTest.kt
+++ codeSnippets/snippets/tutorial-website-interactive/src/test/kotlin/com/example/ApplicationTest.kt
@@ -9,37 +9,25 @@ import kotlin.test.*
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
-        val response = client.get("/")
+        val response = client.get("/static/aboutme.html")
         assertEquals(HttpStatusCode.OK, response.status)
         assertEquals(
             """
-                        <!DOCTYPE html>
-                        <html lang="en">
-                        <head>
-                            <title>Kotlin Journal</title>
-                        </head>
-                        <body style="text-align: center; font-family: sans-serif">
-                        <img src="/static/ktor_logo.png">
-                        <h1>Kotlin Ktor Journal </h1>
-                        <p><i>Powered by Ktor & Freemarker!</i></p>
-                        <hr>
-                            <div>
-                                <h3>The drive to develop!</h3>
-                                <p>...it&#39;s what keeps me going.</p>
-                            </div>
-                        <hr>
-                        <div>
-                            <h3>Add a new journal entry!</h3>
-                            <form action="/submit" method="post">
-                                <input type="text" name="title">
-                                <br>
-                                <textarea name="body"></textarea>
-                                <br>
-                                <input type="submit">
-                            </form>
-                        </div>
-                        </body>
-                        </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kotlin Journal</title>
+</head>
+<body>
+<body style="text-align: center; font-family: sans-serif">
+<img src="/static/ktor_logo.png" alt="ktor logo">
+<h1>About me</h1>
+<p>Welcome to my static page!</p>
+<p>Feel free to take a look around.</p>
+<p>Or go to the <a href="/">main page</a>.</p>
+</body>
+</html>
                     """.trimIndent(),
             response.bodyAsText()
         )
