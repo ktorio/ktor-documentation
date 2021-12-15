946c4777866daf29bef50642773ecb28c0f2c0eb
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-12-15 17:06:47 +0300

    KTOR-3429 hardcode properties

--- codeSnippets/snippets/tutorial-website-interactive-persistence/src/main/kotlin/com/example/dao/DatabaseFactory.kt
+++ codeSnippets/snippets/tutorial-website-interactive-persistence/src/main/kotlin/com/example/dao/DatabaseFactory.kt
@@ -1,22 +1,17 @@
 package com.example.dao
 
 import com.example.models.*
-import io.ktor.server.config.*
 import kotlinx.coroutines.*
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.*
 import org.jetbrains.exposed.sql.transactions.experimental.*
-import java.io.*
 
 object DatabaseFactory {
-    fun init(config: ApplicationConfig) {
-        val driverClassName = config.property("storage.driverClassName").getString()
-        val jdbcURL = config.property("storage.jdbcURL").getString() +
-                (config.propertyOrNull("storage.dbFilePath")?.getString()?.let {
-                    File(it).canonicalFile.absolutePath
-                } ?: "")
-        Database.connect(jdbcURL, driverClassName)
-        transaction {
+    fun init() {
+        val driverClassName = "org.h2.Driver"
+        val jdbcURL = "jdbc:h2:file:./build/db"
+        val database = Database.connect(jdbcURL, driverClassName)
+        transaction(database) {
             SchemaUtils.create(Articles)
         }
     }
