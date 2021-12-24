f419e139b4f265c2210124043376da629cd9c19a
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-12-24 12:45:16 +0300

    KTOR-1885 webjars sample

---
+++ codeSnippets/snippets/webjars/src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,19 @@
+package com.example
+
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.routing.*
+import io.ktor.server.webjars.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    install(Webjars) {
+        path = "assets"
+    }
+    routing {
+        static("/static") {
+            resources("files")
+        }
+    }
+}
