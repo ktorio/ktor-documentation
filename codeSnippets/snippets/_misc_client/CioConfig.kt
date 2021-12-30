740ac9a50ecdaa40b14c85c7ca11df436e7dcdf9
andreyaksenov <andrey.aksenov@jetbrains.com>
2021-12-30 16:14:39 +0300

    fix api links

--- codeSnippets/snippets/_misc_client/CioConfig.kt
+++ codeSnippets/snippets/_misc_client/CioConfig.kt
@@ -7,7 +7,7 @@ val client = HttpClient(CIO) {
         // this: [[[CIOEngineConfig|https://api.ktor.io/ktor-client/ktor-client-cio/io.ktor.client.engine.cio/-c-i-o-engine-config/index.html]]]
         maxConnectionsCount = 1000
         endpoint {
-            // this: [[[EndpointConfig|https://api.ktor.io/ktor-client/ktor-client-cio/ktor-client-cio/io.ktor.client.engine.cio/-endpoint-config/index.html]]]
+            // this: [[[EndpointConfig|https://api.ktor.io/ktor-client/ktor-client-cio/io.ktor.client.engine.cio/-endpoint-config/index.html]]]
             maxConnectionsPerRoute = 100
             pipelineMaxSize = 20
             keepAliveTime = 5000
@@ -15,7 +15,7 @@ val client = HttpClient(CIO) {
             connectAttempts = 5
         }
         https {
-            // this: [[[TLSConfigBuilder|https://api.ktor.io/ktor-network/ktor-network-tls/ktor-network-tls/io.ktor.network.tls/-t-l-s-config-builder/index.html]]]
+            // this: [[[TLSConfigBuilder|https://api.ktor.io/ktor-network/ktor-network-tls/io.ktor.network.tls/-t-l-s-config-builder/index.html]]]
             serverName = "api.ktor.io"
             cipherSuites = CIOCipherSuites.SupportedSuites
             trustManager = myCustomTrustManager
