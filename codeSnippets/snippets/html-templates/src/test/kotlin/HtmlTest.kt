9c386f2a970f8aea829bedc29aebea9f7685b790
andreyaksenov <andrey.aksenov@jetbrains.com>
2022-01-19 14:41:31 +0300

    KTOR-3708 add respondHtmlTemplate sample

---
+++ codeSnippets/snippets/html-templates/src/test/kotlin/HtmlTest.kt
@@ -0,0 +1,28 @@
+package com.example
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.server.testing.*
+import org.junit.Test
+import kotlin.test.*
+
+class HtmlTest {
+    @Test
+    fun testRoot() = testApplication {
+        val response = client.get("/")
+        assertEquals(
+            """
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Ktor</h1>
+    <article>
+      <h2>Hello from Ktor!</h2>
+      <p>Kotlin Framework for creating connected systems.</p>
+    </article>
+  </body>
+</html>
+
+            """.trimIndent(), response.bodyAsText())
+    }
+}
