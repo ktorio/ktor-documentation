00549ea415a91e0d00c0d19ed053268a2154c060
andreyaksenov <andrey.aksenov@jetbrains.com>
2022-02-18 15:13:18 +0300

    KTOR-2888 KMM tutorial

---
+++ codeSnippets/snippets/tutorial-client-kmm/androidApp/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    compileSdk = 31
+    defaultConfig {
+        applicationId = "com.example.kmmktor.android"
+        minSdk = 23
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":shared"))
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
+}
