58a4769499105fac4ef0cbc87653c67a2dc197d3
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-04-01 10:37:12 +0300

    update version

--- codeSnippets/snippets/tutorial-client-kmm/shared/build.gradle.kts
+++ codeSnippets/snippets/tutorial-client-kmm/shared/build.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
     }
     
     sourceSets {
-        val ktorVersion = "2.0.0-eap-355"
+        val ktorVersion = "2.0.0-eap-361"
         val commonMain by getting {
             dependencies {
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
