4f746f3a9c78aea90870b5af7f7fdac36660f3fc
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-04-04 15:38:23 +0300

    KTOR-4108 update serialization plugin

--- codeSnippets/snippets/client-validate-2xx-response/build.gradle.kts
+++ codeSnippets/snippets/client-validate-2xx-response/build.gradle.kts
@@ -5,7 +5,7 @@ val junit_version: String by project
 plugins {
     application
     kotlin("jvm")
-    kotlin("plugin.serialization").version("1.6.0")
+    kotlin("plugin.serialization").version("1.6.20")
 }
 
 application {
