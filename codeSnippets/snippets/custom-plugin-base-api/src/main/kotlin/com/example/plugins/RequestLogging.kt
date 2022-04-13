d33c8d41b54f90583c33e7580e58816df9a304da
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-04-13 12:21:28 +0300

    KTOR-3848 inline intercept

--- codeSnippets/snippets/custom-plugin-base-api/src/main/kotlin/com/example/plugins/RequestLogging.kt
+++ codeSnippets/snippets/custom-plugin-base-api/src/main/kotlin/com/example/plugins/RequestLogging.kt
@@ -4,21 +4,16 @@ import io.ktor.serialization.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.*
 import io.ktor.util.*
-import io.ktor.util.pipeline.*
 
-class RequestLogging() {
-    private fun intercept(context: PipelineContext<Unit, ApplicationCall>) {
-        context.call.request.origin.apply {
-            println("Request URL: $scheme://$host:$port$uri")
-        }
-    }
-
+class RequestLogging {
     companion object Plugin : BaseApplicationPlugin<ApplicationCallPipeline, Configuration, RequestLogging> {
         override val key = AttributeKey<RequestLogging>("RequestLogging")
         override fun install(pipeline: ApplicationCallPipeline, configure: Configuration.() -> Unit): RequestLogging {
             val plugin = RequestLogging()
             pipeline.intercept(ApplicationCallPipeline.Monitoring) {
-                plugin.intercept(this)
+                call.request.origin.apply {
+                    println("Request URL: $scheme://$host:$port$uri")
+                }
             }
             return plugin
         }
