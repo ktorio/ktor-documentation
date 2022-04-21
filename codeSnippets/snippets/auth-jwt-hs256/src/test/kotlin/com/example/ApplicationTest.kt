f096a4964a2e246777d7e600a9ffb85e79f38391
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-04-21 15:25:16 +0300

    KTOR-2730 add jwt test

---
+++ codeSnippets/snippets/auth-jwt-hs256/src/test/kotlin/com/example/ApplicationTest.kt
@@ -0,0 +1,36 @@
+package com.example
+
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.testing.*
+import kotlinx.serialization.*
+import kotlin.test.*
+
+class ApplicationTest {
+    @Test
+    fun testHello() = testApplication {
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+        val loginResponse = client.post("login") {
+            contentType(ContentType.Application.Json)
+            setBody(User("jetbrains", "foobar"))
+        }
+        val authToken = loginResponse.body<AuthToken>().token
+        val helloResponseText = client.get("hello") {
+            header(HttpHeaders.Authorization, "Bearer $authToken")
+        }.bodyAsText()
+        assertTrue {
+            helloResponseText.contains("Hello, jetbrains!")
+        }
+    }
+}
+
+@Serializable
+data class AuthToken(val token: String)
