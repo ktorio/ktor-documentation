337e85513ea3c95b347f304cd15df2a1c2ea324c
aSemy <897017+aSemy@users.noreply.github.com>
2022-06-30 22:03:22 +0200

    change AtomicInteger var to val

--- codeSnippets/snippets/server-websockets/src/main/kotlin/com/example/Application.kt
+++ codeSnippets/snippets/server-websockets/src/main/kotlin/com/example/Application.kt
@@ -62,7 +62,7 @@ fun Application.module() {
 
 class Connection(val session: DefaultWebSocketSession) {
     companion object {
-        var lastId = AtomicInteger(0)
+        val lastId = AtomicInteger(0)
     }
 
     val name = "user${lastId.getAndIncrement()}"
