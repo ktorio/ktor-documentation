d37e9e7fef30ab6ac44baaff1b927ecd7cba1b35
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-07-11 16:32:07 +0400

    KTOR-4585 disable darwin sample for windows/linux

--- codeSnippets/snippets/client-engine-darwin/build.gradle.kts
+++ codeSnippets/snippets/client-engine-darwin/build.gradle.kts
@@ -13,12 +13,8 @@ repositories {
 }
 
 kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
+    val nativeTarget = when (System.getProperty("os.name")) {
+        "Mac OS X" -> macosX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
