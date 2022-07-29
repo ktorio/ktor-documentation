cb68d947ccc802c04d81ae24e7fe2920d2161786
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-07-29 18:32:16 +0400

    KTOR-4285 events sample

---
+++ codeSnippets/snippets/events/src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,30 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    install(ApplicationMonitoringPlugin)
+    environment.monitor.subscribe(ApplicationStarted) { application ->
+        log.info("Server is started")
+    }
+    environment.monitor.subscribe(ApplicationStopped) { application ->
+        log.info("Server is stopped")
+        // Release resources and unsubscribe from events
+        application.environment.monitor.unsubscribe(ApplicationStarted) {}
+        application.environment.monitor.unsubscribe(ApplicationStopped) {}
+    }
+    environment.monitor.subscribe(NotFoundEvent) { call ->
+        log.info("No page was found for the URI: ${call.request.uri}")
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+    }
+}
