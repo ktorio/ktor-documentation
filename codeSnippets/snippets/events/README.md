9337f8ad0d306cb3501a2a9fc7ebb39556d71458
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-07-29 18:32:16 +0400

    KTOR-4285 events sample

---
+++ codeSnippets/snippets/events/README.md
@@ -0,0 +1,10 @@
+# Application monitoring
+
+A sample Ktor project showing how to use [events](https://ktor.io/docs/events.html) to monitor a Ktor server application.
+
+## Running
+
+To run a sample, execute the following command in a repository's root directory:
+```bash
+./gradlew :events:run
+```
---
+++ codeSnippets/snippets/events/src/main/kotlin/com/example/plugins/ApplicationMonitoringPlugin.kt
@@ -0,0 +1,25 @@
+package com.example.plugins
+
+import io.ktor.events.EventDefinition
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.application.hooks.*
+
+val ApplicationMonitoringPlugin = createApplicationPlugin(name = "ApplicationMonitoringPlugin") {
+    on(MonitoringEvent(ApplicationStarted)) { application ->
+        application.log.info("Server is started")
+    }
+    on(MonitoringEvent(ApplicationStopped)) { application ->
+        application.log.info("Server is stopped")
+        // Release resources and unsubscribe from events
+        application.environment.monitor.unsubscribe(ApplicationStarted) {}
+        application.environment.monitor.unsubscribe(ApplicationStopped) {}
+    }
+    on(ResponseSent) { call ->
+        if (call.response.status() == HttpStatusCode.NotFound) {
+            this@createApplicationPlugin.application.environment.monitor.raise(NotFoundEvent, call)
+        }
+    }
+}
+
+val NotFoundEvent: EventDefinition<ApplicationCall> = EventDefinition()
