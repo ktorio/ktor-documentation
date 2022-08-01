c0164b53c3ae678a883b36c65536c43fa72998bb
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-08-01 11:55:44 +0400

    KTOR-4490 update plugin version

--- codeSnippets/snippets/tutorial-website-interactive-docker-compose/build.gradle.kts
+++ codeSnippets/snippets/tutorial-website-interactive-docker-compose/build.gradle.kts
@@ -7,7 +7,7 @@ val logback_version: String by project
 plugins {
     application
     kotlin("jvm")
-    id("io.ktor.plugin") version "2.1.0-eap-21"
+    id("io.ktor.plugin") version "2.1.0"
 }
 
 application {
