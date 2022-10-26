968dbd8236344638480738e522aa01bb7c8d16e9
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2022-10-26 16:57:09 +0400

    KTOR-2434 use explicit imports

--- codeSnippets/snippets/mustache/src/main/kotlin/com/example/Application.kt
+++ codeSnippets/snippets/mustache/src/main/kotlin/com/example/Application.kt
@@ -2,7 +2,8 @@ package com.example
 
 import com.github.mustachejava.DefaultMustacheFactory
 import io.ktor.server.application.*
-import io.ktor.server.mustache.*
+import io.ktor.server.mustache.Mustache
+import io.ktor.server.mustache.MustacheContent
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
