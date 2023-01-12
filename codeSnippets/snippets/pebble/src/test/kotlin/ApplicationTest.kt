a7b92bd34644ab06311c8f979381903c81d6c472
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2023-01-12 11:37:09 +0400

    KTOR-5430 add tests

---
+++ codeSnippets/snippets/pebble/src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,21 @@
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.server.testing.*
+import org.junit.*
+import org.junit.Assert.*
+
+class ApplicationTest {
+    @Test
+    fun test() = testApplication {
+        val response: String = client.get("/index").body()
+        assertEquals("""
+<html>
+    <body>
+        <h1>Hello, John</h1>
+    </body>
+</html>
+        """.trimIndent(),
+            response
+        )
+    }
+}
