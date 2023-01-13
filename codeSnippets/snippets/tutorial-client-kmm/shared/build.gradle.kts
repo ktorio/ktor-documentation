7089cd73b1b809d9db4fedef30a005bd897d2210
Andrey.Aksenov <Andrey.Aksenov@jetbrains.com>
2023-01-13 09:28:40 +0400

    KTOR-5436 update to 2.3.0

--- codeSnippets/snippets/tutorial-client-kmm/shared/build.gradle.kts
+++ codeSnippets/snippets/tutorial-client-kmm/shared/build.gradle.kts
@@ -17,7 +17,7 @@ kotlin {
     }
 
     sourceSets {
-        val ktorVersion = "2.2.2"
+        val ktorVersion = "2.3.0"
         val commonMain by getting {
             dependencies {
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
