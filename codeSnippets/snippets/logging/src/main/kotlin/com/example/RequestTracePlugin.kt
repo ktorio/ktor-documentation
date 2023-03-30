cbe1680ec652ba7e658cf0dfa85f16915a4ff06a
Leonid Stashevsky <e5l@users.noreply.github.com>
2023-03-30 11:43:13 +0300

    KTOR-5530 Describe Logging in Plugins and Files (#294)

---
+++ codeSnippets/snippets/logging/src/main/kotlin/com/example/RequestTracePlugin.kt
@@ -0,0 +1,13 @@
+package com.example
+
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.util.logging.*
+
+internal val LOGGER = KtorSimpleLogger("com.example.RequestTracePlugin")
+
+val RequestTracePlugin = createRouteScopedPlugin("RequestTracePlugin", { }) {
+    onCall { call ->
+        LOGGER.trace("Processing call: ${call.request.uri}")
+    }
+}
