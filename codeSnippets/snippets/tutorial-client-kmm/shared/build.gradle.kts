53758221361429035443e75ef88c24d4c050e57c
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2023-06-29 13:13:43 +0300

    KTOR-6059 Release docs for 2.3.2 (#317)

--- codeSnippets/snippets/tutorial-client-kmm/shared/build.gradle.kts
+++ codeSnippets/snippets/tutorial-client-kmm/shared/build.gradle.kts
@@ -17,7 +17,7 @@ kotlin {
     }
 
     sourceSets {
-        val ktorVersion = "2.3.1"
+        val ktorVersion = "2.3.2"
         val commonMain by getting {
             dependencies {
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
