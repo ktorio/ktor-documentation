e268948f16898e01236f3a616ea9e94e74e02291
Stexxe <aleksei.tirman@jetbrains.com>
2023-08-02 12:24:54 +0300

    KTOR-6169 Release docs for 2.3.3

--- codeSnippets/snippets/proguard/build.gradle.kts
+++ codeSnippets/snippets/proguard/build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
 plugins {
     application
     kotlin("jvm")
-    id("io.ktor.plugin") version "2.3.2"
+    id("io.ktor.plugin") version "2.3.3"
 }
 
 application {
--- codeSnippets/snippets/tutorial-client-kmm/shared/build.gradle.kts
+++ codeSnippets/snippets/tutorial-client-kmm/shared/build.gradle.kts
@@ -17,7 +17,7 @@ kotlin {
     }
 
     sourceSets {
-        val ktorVersion = "2.3.2"
+        val ktorVersion = "2.3.3"
         val commonMain by getting {
             dependencies {
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
