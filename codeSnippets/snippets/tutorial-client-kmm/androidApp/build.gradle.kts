2e7caf003d8e873ec6ffc3acbeaa9cd3e603d456
Viktoriya Nikolova <contact@viknikolova.com>
2023-09-20 15:36:33 +0200

    upgrade kotlinCompilerExtensionVersion to fix build for Android

--- codeSnippets/snippets/tutorial-client-kmm/androidApp/build.gradle.kts
+++ codeSnippets/snippets/tutorial-client-kmm/androidApp/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.0"
+        kotlinCompilerExtensionVersion = "1.4.0"
     }
     packagingOptions {
         resources {
