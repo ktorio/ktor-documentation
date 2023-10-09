1e1789804673399b4a673d344d8f583fb702a220
Vik Nikolova <contact@viknikolova.com>
2023-10-09 10:22:25 +0200

    KTOR-6351 update versions for release 2.3.5 (#349)

--- codeSnippets/snippets/tutorial-client-kmm/shared/build.gradle.kts
+++ codeSnippets/snippets/tutorial-client-kmm/shared/build.gradle.kts
@@ -17,7 +17,7 @@ kotlin {
     }
 
     sourceSets {
-        val ktorVersion = "2.3.4"
+        val ktorVersion = "2.3.5"
         val commonMain by getting {
             dependencies {
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
