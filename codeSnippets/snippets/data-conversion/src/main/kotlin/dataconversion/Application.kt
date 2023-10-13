63a358d716212a5a797ca6c2f9b826cece6b9f4c
Viktoriya Nikolova <contact@viknikolova.com>
2023-10-13 14:03:24 +0200

    KTOR-3623 and KTOR-4870 update docs for data conversion and add example project

---
+++ codeSnippets/snippets/data-conversion/src/main/kotlin/dataconversion/Application.kt
@@ -0,0 +1,61 @@
+package dataconversion
+
+import io.ktor.server.application.*
+import io.ktor.server.locations.*
+import io.ktor.server.plugins.dataconversion.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.util.reflect.*
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatterBuilder
+import java.time.format.SignStyle
+import java.time.temporal.ChronoField
+import java.util.*
+
+enum class ArticleEnum {
+    A, B, C
+}
+
+@Location("/")
+class LocationWithEnum(val article: ArticleEnum)
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    install(Locations)
+    install(DataConversion) {
+        convert<LocalDate> { // this: DelegatingConversionService
+            val formatter = DateTimeFormatterBuilder()
+                .appendValue(ChronoField.YEAR, 4, 4, SignStyle.NEVER)
+                .appendValue(ChronoField.MONTH_OF_YEAR, 2)
+                .appendValue(ChronoField.DAY_OF_MONTH, 2)
+                .toFormatter(Locale.ROOT)
+
+            decode { values -> // converter: (values: List<String>) -> Any?
+                LocalDate.from(formatter.parse(values.single()))
+            }
+
+            encode { value -> // converter: (value: Any?) -> List<String>
+                listOf(SimpleDateFormat.getInstance().format(value))
+            }
+        }
+        convert<ArticleEnum> {
+            decode { values ->
+                ArticleEnum.entries.first { it.name.lowercase() in values }
+            }
+            encode { value -> listOf(value.name.lowercase()) }
+        }
+    }
+
+    routing {
+        get<LocationWithEnum> { data ->
+            call.respondText("The received article is ${data.article.name}")
+        }
+        get("/date") {
+            val encodedDate = application.conversionService.toValues(call.parameters["date"])
+            val decodedDate = application.conversionService.fromValues(encodedDate, typeInfo<LocalDate>())
+            call.respondText("The date is $decodedDate")
+        }
+    }
+}
