e58d5c7df90883141e9c1f010df5abc3f7ae0d1a
Max McCord <maxmccord93@gmail.com>
2023-11-29 15:17:26 -0800

    Update DatabaseSingleton.kt to use companion object
    Found this issue while following the persistence tutorial: https://ktor.io/docs/interactive-website-add-persistence.html#connect_db
    
    As previously written, the code does not compile. Static functions should be placed within a companion object.

--- codeSnippets/snippets/tutorial-website-interactive-persistence/src/main/kotlin/com/example/dao/DatabaseSingleton.kt
+++ codeSnippets/snippets/tutorial-website-interactive-persistence/src/main/kotlin/com/example/dao/DatabaseSingleton.kt
@@ -7,15 +7,17 @@ import org.jetbrains.exposed.sql.transactions.*
 import org.jetbrains.exposed.sql.transactions.experimental.*
 
 object DatabaseSingleton {
-    fun init() {
-        val driverClassName = "org.h2.Driver"
-        val jdbcURL = "jdbc:h2:file:./build/db"
-        val database = Database.connect(jdbcURL, driverClassName)
-        transaction(database) {
-            SchemaUtils.create(Articles)
+    companion object {
+        fun init() {
+            val driverClassName = "org.h2.Driver"
+            val jdbcURL = "jdbc:h2:file:./build/db"
+            val database = Database.connect(jdbcURL, driverClassName)
+            transaction(database) {
+                SchemaUtils.create(Articles)
+            }
         }
+    
+        suspend fun <T> dbQuery(block: suspend () -> T): T =
+            newSuspendedTransaction(Dispatchers.IO) { block() }
     }
-
-    suspend fun <T> dbQuery(block: suspend () -> T): T =
-        newSuspendedTransaction(Dispatchers.IO) { block() }
 }
