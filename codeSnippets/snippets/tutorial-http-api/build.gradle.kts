8b42ed573e0e2e660554931f7dcdd51f59b9d996
Viktoriya Nikolova <contact@viknikolova.com>
2024-01-31 19:03:39 +0100

    update Ktor version from 2.3.7 to 2.3.8

--- codeSnippets/snippets/tutorial-http-api/build.gradle.kts
+++ codeSnippets/snippets/tutorial-http-api/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     application
     kotlin("jvm")
     kotlin("plugin.serialization") version "1.9.10"
-    id("io.ktor.plugin") version "2.3.7"
+    id("io.ktor.plugin") version "2.3.8"
 }
 
 application {
