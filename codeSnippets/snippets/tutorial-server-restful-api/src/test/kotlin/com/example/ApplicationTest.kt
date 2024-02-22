b29198e66cf4b9f8fb42a18db28452f4e948bcc0
Viktoriya Nikolova <contact@viknikolova.com>
2024-02-22 12:11:30 +0100

    move the model package in the code example within com/example

--- codeSnippets/snippets/tutorial-server-restful-api/src/test/kotlin/com/example/ApplicationTest.kt
+++ codeSnippets/snippets/tutorial-server-restful-api/src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,12 +1,13 @@
 package com.example
 
+import com.example.model.Priority
+import com.example.model.Task
 import io.ktor.client.call.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
-import model.*
 import kotlin.test.*
 
 class ApplicationTest {
