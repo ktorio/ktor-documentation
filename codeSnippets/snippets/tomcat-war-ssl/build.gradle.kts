f63e26add985fc8e862afc2359c79a65e81d1f83
Viktoriya Nikolova <contact@viknikolova.com>
2024-03-14 18:26:06 +0100

    update versions in tomcat-war-ssl code example

--- codeSnippets/snippets/tomcat-war-ssl/build.gradle.kts
+++ codeSnippets/snippets/tomcat-war-ssl/build.gradle.kts
@@ -5,12 +5,12 @@ val slf4j_version: String by project
 plugins {
     application
     kotlin("jvm")
-    id("org.gretty") version "3.1.2"
+    id("org.gretty") version "4.0.3"
     id("war")
 }
 
 gretty {
-    servletContainer = "tomcat9"
+    servletContainer = "tomcat10"
     contextPath = "/"
     logbackConfigFile = "src/main/resources/logback.xml"
     httpsEnabled = true
