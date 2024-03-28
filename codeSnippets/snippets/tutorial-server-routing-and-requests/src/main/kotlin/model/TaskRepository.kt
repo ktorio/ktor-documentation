db5a04cb4fe9b17dd2ce018b323470b1caef5ebc
Viktoriya Nikolova <contact@viknikolova.com>
2024-03-28 18:32:22 +0100

    add spacing between functions in TaskRepository.kt

--- codeSnippets/snippets/tutorial-server-routing-and-requests/src/main/kotlin/model/TaskRepository.kt
+++ codeSnippets/snippets/tutorial-server-routing-and-requests/src/main/kotlin/model/TaskRepository.kt
@@ -9,12 +9,15 @@ object TaskRepository {
     )
 
     fun allTasks(): List<Task> = tasks
+
     fun tasksByPriority(priority: Priority) = tasks.filter {
         it.priority == priority
     }
+
     fun taskByName(name: String) = tasks.find {
         it.name.equals(name, ignoreCase = true)
     }
+
     fun addTask(task: Task) {
         if(taskByName(task.name) != null) {
             throw IllegalStateException("Cannot duplicate task names!")
