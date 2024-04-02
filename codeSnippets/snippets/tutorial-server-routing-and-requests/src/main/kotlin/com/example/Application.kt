67a851a6ae1f9cfe1334e193e51234b0a1048623
Vik Nikolova <contact@viknikolova.com>
2024-04-02 14:48:36 +0200

    Add a new tutorial "Handle requests and generate responses" (#421)
    * add 2 new topics and add content for "Handle requests and generate responses"
    * remove the "Creating HTTP API's" tutorial
    * add an example project in codeSnippets

---
+++ codeSnippets/snippets/tutorial-server-routing-and-requests/src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
