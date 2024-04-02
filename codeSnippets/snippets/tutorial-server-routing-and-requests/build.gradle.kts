0cac10ddf6e122b81c01da26bd4b16074da1e83b
Vik Nikolova <contact@viknikolova.com>
2024-04-02 14:48:36 +0200

    Add a new tutorial "Handle requests and generate responses" (#421)
    * add 2 new topics and add content for "Handle requests and generate responses"
    * remove the "Creating HTTP API's" tutorial
    * add an example project in codeSnippets

---
+++ codeSnippets/snippets/tutorial-server-routing-and-requests/build.gradle.kts
@@ -0,0 +1,27 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    application
+    kotlin("jvm")
+    id("io.ktor.plugin") version "2.3.8"
+}
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
