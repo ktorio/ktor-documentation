8768833a4bf2b5fafc7afc9c41f93abcced5d12f
Vik Nikolova <contact@viknikolova.com>
2024-04-02 14:48:36 +0200

    Add a new tutorial "Handle requests and generate responses" (#421)
    * add 2 new topics and add content for "Handle requests and generate responses"
    * remove the "Creating HTTP API's" tutorial
    * add an example project in codeSnippets

---
+++ codeSnippets/snippets/tutorial-server-routing-and-requests/src/main/kotlin/model/TaskRepository.kt
@@ -0,0 +1,27 @@
+package model
+
+object TaskRepository {
+    private val tasks = mutableListOf(
+        Task("cleaning", "Clean the house", Priority.Low),
+        Task("gardening", "Mow the lawn", Priority.Medium),
+        Task("shopping", "Buy the groceries", Priority.High),
+        Task("painting", "Paint the fence", Priority.Medium)
+    )
+
+    fun allTasks(): List<Task> = tasks
+
+    fun tasksByPriority(priority: Priority) = tasks.filter {
+        it.priority == priority
+    }
+
+    fun taskByName(name: String) = tasks.find {
+        it.name.equals(name, ignoreCase = true)
+    }
+
+    fun addTask(task: Task) {
+        if(taskByName(task.name) != null) {
+            throw IllegalStateException("Cannot duplicate task names!")
+        }
+        tasks.add(task)
+    }
+}
---
+++ codeSnippets/snippets/tutorial-server-routing-and-requests/src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,85 @@
+package com.example
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+
+
+class ApplicationTest {
+    @Test
+    fun tasksCanBeFoundByPriority() = testApplication {
+        application {
+            module()
+        }
+
+
+        val response = client.get("/tasks/byPriority/Medium")
+        val body = response.bodyAsText()
+
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertContains(body, "Mow the lawn")
+        assertContains(body, "Paint the fence")
+    }
+
+
+    @Test
+    fun invalidPriorityProduces400() = testApplication {
+        application {
+            module()
+        }
+
+
+        val response = client.get("/tasks/byPriority/Invalid")
+        assertEquals(HttpStatusCode.BadRequest, response.status)
+    }
+
+
+    @Test
+    fun unusedPriorityProduces404() = testApplication {
+        application {
+            module()
+        }
+
+
+        val response = client.get("/tasks/byPriority/Vital")
+        assertEquals(HttpStatusCode.NotFound, response.status)
+    }
+
+    @Test
+    fun newTasksCanBeAdded() = testApplication {
+        application {
+            module()
+        }
+
+
+        val response1 = client.post("/tasks") {
+            header(
+                HttpHeaders.ContentType,
+                ContentType.Application.FormUrlEncoded.toString()
+            )
+            setBody(
+                listOf(
+                    "name" to "swimming",
+                    "description" to "Go to the beach",
+                    "priority" to "Low"
+                ).formUrlEncode()
+            )
+        }
+        assertEquals(HttpStatusCode.NoContent, response1.status)
+
+
+        val response2 = client.get("/tasks")
+        assertEquals(HttpStatusCode.OK, response2.status)
+        val body = response2.bodyAsText()
+
+
+        assertContains(body, "swimming")
+        assertContains(body, "Go to the beach")
+    }
+
+}
