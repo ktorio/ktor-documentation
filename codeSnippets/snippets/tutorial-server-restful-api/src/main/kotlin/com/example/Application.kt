7c5a49fa5b1cad3e423182e73b707c80dfec51ef
Vik Nikolova <contact@viknikolova.com>
2024-04-04 18:12:22 +0200

    KTOR-6741 Part 2: Add a new tutorial Create RESTful APIs (#459)

---
+++ codeSnippets/snippets/tutorial-server-restful-api/src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,13 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
---
+++ codeSnippets/snippets/tutorial-server-restful-api/src/test/kotlin/com/example/ApplicationTest.kt
@@ -0,0 +1,73 @@
+package com.example
+
+import com.example.model.Priority
+import com.example.model.Task
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class ApplicationTest {
+    @Test
+    fun tasksCanBeFoundByPriority() = testApplication {
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        val response = client.get("/tasks/byPriority/Medium")
+        val results = response.body<List<Task>>()
+
+        assertEquals(HttpStatusCode.OK, response.status)
+
+        val expectedTaskNames = listOf("gardening", "painting")
+        val actualTaskNames = results.map(Task::name)
+        assertContentEquals(expectedTaskNames, actualTaskNames)
+    }
+
+    @Test
+    fun invalidPriorityProduces400() = testApplication {
+        val response = client.get("/tasks/byPriority/Invalid")
+        assertEquals(HttpStatusCode.BadRequest, response.status)
+    }
+
+
+    @Test
+    fun unusedPriorityProduces404() = testApplication {
+        val response = client.get("/tasks/byPriority/Vital")
+        assertEquals(HttpStatusCode.NotFound, response.status)
+    }
+
+    @Test
+    fun newTasksCanBeAdded() = testApplication {
+        val client = createClient {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
+
+        val task = Task("swimming", "Go to the beach", Priority.Low)
+        val response1 = client.post("/tasks") {
+            header(
+                HttpHeaders.ContentType,
+                ContentType.Application.Json
+            )
+
+            setBody(task)
+        }
+        assertEquals(HttpStatusCode.NoContent, response1.status)
+
+        val response2 = client.get("/tasks")
+        assertEquals(HttpStatusCode.OK, response2.status)
+
+        val taskNames = response2
+            .body<List<Task>>()
+            .map { it.name }
+
+        assertContains(taskNames, "swimming")
+    }
+}
