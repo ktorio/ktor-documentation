91a35b600e37a43b59edf3f2d12d585137aa352c
Vik Nikolova <contact@viknikolova.com>
2024-05-03 12:00:03 +0200

    KTOR-6744 Add a new tutorial 'Integrate a database with Exposed' (#466)
    * add content with code example and screenshots
    
    * add links to the previous tutorials and the welcome page

---
+++ codeSnippets/snippets/tutorial-server-db-integration/src/main/kotlin/com/example/plugins/Databases.kt
@@ -0,0 +1,126 @@
+package com.example.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.sql.*
+import kotlinx.coroutines.*
+import org.jetbrains.exposed.sql.*
+
+fun Application.configureDatabases() {
+    val dbConnection: Connection = connectToPostgres(embedded = true)
+    val cityService = CityService(dbConnection)
+    
+    routing {
+    
+        // Create city
+        post("/cities") {
+            val city = call.receive<City>()
+            val id = cityService.create(city)
+            call.respond(HttpStatusCode.Created, id)
+        }
+    
+        // Read city
+        get("/cities/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+            try {
+                val city = cityService.read(id)
+                call.respond(HttpStatusCode.OK, city)
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+    
+        // Update city
+        put("/cities/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+            val user = call.receive<City>()
+            cityService.update(id, user)
+            call.respond(HttpStatusCode.OK)
+        }
+    
+        // Delete city
+        delete("/cities/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+            cityService.delete(id)
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+    val database = Database.connect(
+            url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
+            user = "root",
+            driver = "org.h2.Driver",
+            password = ""
+        )
+    val userService = UserService(database)
+    routing {
+        // Create user
+        post("/users") {
+            val user = call.receive<ExposedUser>()
+            val id = userService.create(user)
+            call.respond(HttpStatusCode.Created, id)
+        }
+        
+            // Read user
+        get("/users/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+            val user = userService.read(id)
+            if (user != null) {
+                call.respond(HttpStatusCode.OK, user)
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+        
+            // Update user
+        put("/users/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+            val user = call.receive<ExposedUser>()
+            userService.update(id, user)
+            call.respond(HttpStatusCode.OK)
+        }
+        
+            // Delete user
+        delete("/users/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+            userService.delete(id)
+            call.respond(HttpStatusCode.OK)
+        }
+    }
+}
+
+/**
+ * Makes a connection to a Postgres database.
+ *
+ * In order to connect to your running Postgres process,
+ * please specify the following parameters in your configuration file:
+ * - postgres.url -- Url of your running database process.
+ * - postgres.user -- Username for database connection
+ * - postgres.password -- Password for database connection
+ *
+ * If you don't have a database process running yet, you may need to [download]((https://www.postgresql.org/download/))
+ * and install Postgres and follow the instructions [here](https://postgresapp.com/).
+ * Then, you would be able to edit your url,  which is usually "jdbc:postgresql://host:port/database", as well as
+ * user and password values.
+ *
+ *
+ * @param embedded -- if [true] defaults to an embedded database for tests that runs locally in the same process.
+ * In this case you don't have to provide any parameters in configuration file, and you don't have to run a process.
+ *
+ * @return [Connection] that represent connection to the database. Please, don't forget to close this connection when
+ * your application shuts down by calling [Connection.close]
+ * */
+fun Application.connectToPostgres(embedded: Boolean): Connection {
+    Class.forName("org.postgresql.Driver")
+    if (embedded) {
+        return DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "root", "")
+    } else {
+        val url = environment.config.property("postgres.url").getString()
+        val user = environment.config.property("postgres.user").getString()
+        val password = environment.config.property("postgres.password").getString()
+
+        return DriverManager.getConnection(url, user, password)
+    }
+}
