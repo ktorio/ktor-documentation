17eb99c86727c1c023a597b6a38775fb6060c893
Viktoriya Nikolova <contact@viknikolova.com>
2024-05-06 17:02:55 +0300

    update and reformat the docker compose topic

---
+++ codeSnippets/snippets/tutorial-server-docker-compose/src/main/kotlin/com/example/db/mapping.kt
@@ -0,0 +1,34 @@
+package com.example.db
+
+import Task
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+
+object TaskTable : IntIdTable("task") {
+    val name = varchar("name", 50)
+    val description = varchar("description", 50)
+    val priority = varchar("priority", 50)
+}
+
+class TaskDAO(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<TaskDAO>(TaskTable)
+
+    var name by TaskTable.name
+    var description by TaskTable.description
+    var priority by TaskTable.priority
+}
+
+suspend fun <T> suspendTransaction(block: Transaction.() -> T): T =
+    newSuspendedTransaction(Dispatchers.IO, statement = block)
+
+
+fun daoToModel(dao: TaskDAO) = Task(
+    dao.name,
+    dao.description,
+    Priority.valueOf(dao.priority)
+)
