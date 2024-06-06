70bd6702a4f215670b92cbf3761d6f6f06c8c925
Ellet <ellet@freshplatform.net>
2024-06-07 01:17:18 +0300

    feat: use required java 9 modules, add runMinimizedJar task, avoid using ignorewarnings and dontwarn in Proguard configurations and hardcode the Minimized JAR name

--- codeSnippets/snippets/proguard/README.md
+++ codeSnippets/snippets/proguard/README.md
@@ -3,12 +3,30 @@
 A sample project packed as a JAR using the [Ktor Gradle plugin](https://ktor.io/docs/fatjar.html) and minimized using [ProGuard](https://www.guardsquare.com/manual/home).
 > This sample is a part of the [codeSnippets](../../README.md) Gradle project.
 
+This example is configured to work with Kotlinx coroutines and serialization, required modules that are part of the JDK
+and some libraries that Ktor server depends on.
+
+if you're using additional libraries that use features such as 
+reflections or dynamic class loading, You will get a warning that needs to be solved.
+
+For this example, the Fat JAR size is (15.2 MB) and the minimized JAR (9 MB). This will
+be different depending on your project, dependencies, and Proguard configurations.
+
 ## Running
 
 To build a minimized JAR and run the application, execute the following commands:
+
 ```bash
-./gradlew :proguard:minimizedJar
+./gradlew :proguard:buildMinimizedJar
 java -jar snippets/proguard/build/libs/my-application.min.jar
 ```
 
+or run the JAR directly using a Gradle task:
+
+```bash
+./gradlew :proguard:runMinimizedJar
+```
+
 Then, navigate to [http://localhost:8080/](http://localhost:8080/) to see the sample home page.
+
+> Replace `gradlew` with `gradlew.bat` if you're using **Microsoft Windows**
--- codeSnippets/snippets/proguard/build.gradle.kts
+++ codeSnippets/snippets/proguard/build.gradle.kts
@@ -1,4 +1,4 @@
-import proguard.gradle.*
+import java.io.FileNotFoundException
 
 val ktor_version: String by project
 val kotlin_version: String by project
@@ -9,7 +9,7 @@ buildscript {
         maven("https://plugins.gradle.org/m2")
     }
     dependencies {
-        classpath("com.guardsquare:proguard-gradle:7.4.2")
+        classpath("com.guardsquare:proguard-gradle:7.5.0")
     }
 }
 
@@ -41,25 +41,74 @@ ktor {
     }
 }
 
-task(name = "minimizedJar", type = ProGuardTask::class) {
-    dependsOn("buildFatJar")
-    injars("build/libs/my-application.jar")
-    outjars("build/libs/my-application.min.jar")
+val buildMinimizedJar = tasks.register<proguard.gradle.ProGuardTask>("buildMinimizedJar") {
+    group = "ktor"
+
+    dependsOn(tasks.buildFatJar)
+
+    val fatJarFile = tasks.shadowJar.flatMap { it.archiveFile }
+    val fatJarFileNameWithoutExtension = fatJarFile.get().asFile.nameWithoutExtension
+    val fatJarDestinationDirectory = tasks.shadowJar.get().destinationDirectory
+    injars(fatJarFile)
+    outjars(
+        fatJarDestinationDirectory.file("${fatJarFileNameWithoutExtension}.min.jar")
+    )
+
+    // Automatically handle the Java version of this build.
     val javaHome = System.getProperty("java.home")
     if (System.getProperty("java.version").startsWith("1.")) {
         // Before Java 9, runtime classes are packaged in a single JAR file.
         libraryjars("$javaHome/lib/rt.jar")
     } else {
         // Starting from Java 9, runtime classes are packaged in modular JMOD files.
-        libraryjars(
-            mapOf("jarfilter" to "!**.jar", "filter" to "!module-info.class"),
-            "$javaHome/jmods/java.base.jmod"
+        fun includeJavaModuleFromJDK(jModFileName: String) {
+            val jModFilePath = "$javaHome/jmods/$jModFileName"
+            val jModFile = File(jModFilePath)
+            if (!jModFile.exists()) {
+                throw FileNotFoundException("The '$jModFile' at '$jModFilePath' doesn't exist.")
+            }
+            libraryjars(
+                mapOf("jarfilter" to "!**.jar", "filter" to "!module-info.class"),
+                jModFilePath,
+            )
+        }
+        val javaModules = listOf(
+            "java.base.jmod",
+            // All imports from "org.mozilla.javascript.tools.debugger" use Java Swing/Desktop imports
+            "java.desktop.jmod",
+            // Some libraries depends on Java logging
+            "java.logging.jmod",
+            // Some libraries depends on Java xml
+            "java.xml.jmod",
+            // Needed by some Ktor modules such as "ktor-server-auth-jvm"
+            "java.naming.jmod",
+            // Needed by some Ktor packages such as "io.ktor.util.debug"
+            "java.management.jmod",
+            // Needed by some libraries such as "com.github.fge.jsonschema"
+            "java.scripting.jmod",
+            // All classes in "com.fasterxml.jackson.databind" need this
+            "java.sql.jmod",
+            // Might be needed when using Java engine in Ktor Client
+            "java.net.http.jmod"
         )
+        javaModules.forEach { includeJavaModuleFromJDK(it) }
     }
-    printmapping("build/libs/my-application.map")
-    ignorewarnings()
+
+    // This will include the Kotlin library jars, it will be needed even though Shadow JAR already includes it
+    // to solve all warnings that are related to Kotlin without '-dontwarn kotlin.**'
+    // this will also solve warnings that are coming from some libraries
+    injars(sourceSets.main.get().compileClasspath)
+
+    printmapping(fatJarDestinationDirectory.file("$fatJarFileNameWithoutExtension.map"))
     dontobfuscate()
     dontoptimize()
-    dontwarn()
-    configuration("proguard.pro")
+
+    configuration(file("proguard.pro"))
+}
+
+tasks.register<JavaExec>("runMinimizedJar") {
+    group = "ktor"
+
+    dependsOn(buildMinimizedJar)
+    classpath = files(tasks.shadowJar.get().archiveFile)
 }
