8026557fcad87e747950bd7b56d983db6a1bfae9
Viktoriya Nikolova <contact@viknikolova.com>
2024-07-18 15:48:35 +0200

    chore: update Kotlin version to 2.0.0 and Ktor versions where applicable

--- codeSnippets/snippets/migrating-express-ktor/5_send_response/build.gradle.kts
+++ codeSnippets/snippets/migrating-express-ktor/5_send_response/build.gradle.kts
@@ -4,8 +4,8 @@ val logback_version: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.8.0"
-    kotlin("plugin.serialization").version("1.9.10")
+    kotlin("jvm")
+    kotlin("plugin.serialization").version("2.0.0")
 }
 
 application {
--- codeSnippets/snippets/server-websockets-sharedflow/build.gradle.kts
+++ codeSnippets/snippets/server-websockets-sharedflow/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     application
     kotlin("jvm")
     id("io.ktor.plugin") version "2.3.12"
-    kotlin("plugin.serialization").version("1.9.10")
+    kotlin("plugin.serialization").version("2.0.0")
 }
 
 application {
