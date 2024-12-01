342db08efc83acd3ac6c2cc40165c2146b4bae62
KEZ <yuijin0909@gmail.com>
2024-12-01 14:24:31 +0900

    Fix Test Extension with out configureApi

--- codeSnippets/snippets/tutorial-full-stack-task-manager/integration-test/src/test/kotlin/core/extension/TestExt.kt
+++ codeSnippets/snippets/tutorial-full-stack-task-manager/integration-test/src/test/kotlin/core/extension/TestExt.kt
@@ -3,13 +3,23 @@ package core.extension
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.HttpClientEngineConfig
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.testing.ApplicationTestBuilder
-import org.example.ktor.core.extension.configuredForApi
+import kotlinx.serialization.json.Json
 
 fun ApplicationTestBuilder.createConfiguredClient(
     additionalConfig: HttpClientConfig<out HttpClientEngineConfig>.() -> Unit = {}
 ): HttpClient {
     return createClient {
+        install(ContentNegotiation) {
+            json(Json {
+                encodeDefaults = true
+                isLenient = true
+                coerceInputValues = true
+                ignoreUnknownKeys = true
+            })
+        }
         additionalConfig()
-    }.configuredForApi()
+    }
 }
