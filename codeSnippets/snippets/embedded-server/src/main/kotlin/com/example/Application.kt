023bbe8367cea4f8040bc01667e6c3a505651621
Viktoriya Nikolova <contact@viknikolova.com>
2024-12-11 14:39:03 +0100

    KTOR-7415 extend embedded-server to use connector and update affected topics

--- codeSnippets/snippets/embedded-server/src/main/kotlin/com/example/Application.kt
+++ codeSnippets/snippets/embedded-server/src/main/kotlin/com/example/Application.kt
@@ -1,13 +1,52 @@
 package com.example
 
-import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main() {
+/*
+   Important: This contents of this file are referenced by line number in `server-configuration-code.topic`
+    and `server-engines.md`. If you add, remove, or modify any lines, ensure you update the corresponding
+    line numbers in the `code-block` element of the referenced file.
+*/
+
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        println("Running basic server...")
+        println("Provide the 'configured' argument to run a configured server.")
+        runBasicServer()
+    }
+
+    when (args[0]) {
+        "basic" -> runBasicServer()
+        "configured" -> runConfiguredServer()
+        else -> println("Unknown argument: ${args[0]}. Use 'basic' or 'configured'.")
+    }
+}
+
+fun runBasicServer() {
     embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!")
+            }
+        }
+    }.start(wait = true)
+}
+
+fun runConfiguredServer() {
+    embeddedServer(Netty, configure = {
+        connectors.add(EngineConnectorBuilder().apply {
+            host = "127.0.0.1"
+            port = 8080
+        })
+        connectionGroupSize = 2
+        workerGroupSize = 5
+        callGroupSize = 10
+        shutdownGracePeriod = 2000
+        shutdownTimeout = 3000
+    }) {
         routing {
             get("/") {
                 call.respondText("Hello, world!")
