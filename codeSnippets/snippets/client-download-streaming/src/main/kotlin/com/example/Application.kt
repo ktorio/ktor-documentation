96d2d356a0a716ec1290bdcb951788dad796dbf2
Vik Nikolova <contact@viknikolova.com>
2025-03-18 13:21:49 +0100

    chore: KTOR-7210 Document migration to `kotlinx-io` in the migration guide and update streaming example (#602)

--- codeSnippets/snippets/client-download-streaming/src/main/kotlin/com/example/Application.kt
+++ codeSnippets/snippets/client-download-streaming/src/main/kotlin/com/example/Application.kt
@@ -4,29 +4,34 @@ import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.utils.io.*
 import io.ktor.utils.io.core.*
 import kotlinx.coroutines.runBlocking
+import kotlinx.io.asSink
 import java.io.File
 
 fun main() {
     val client = HttpClient(CIO)
     val file = File.createTempFile("files", "index")
+    val stream = file.outputStream().asSink()
+    val fileSize = 100 * 1024 * 1024
 
     runBlocking {
-        client.prepareGet("https://ktor.io/").execute { httpResponse ->
+        client.prepareGet("https://httpbin.org/bytes/$fileSize").execute { httpResponse ->
             val channel: ByteReadChannel = httpResponse.body()
-            while (!channel.isClosedForRead) {
-                val packet = channel.readRemaining(DEFAULT_BUFFER_SIZE.toLong())
-                while (!packet.isEmpty) {
-                    val bytes = packet.readBytes()
-                    file.appendBytes(bytes)
-                    println("Received ${file.length()} bytes from ${httpResponse.contentLength()}")
+            var count = 0L
+            stream.use {
+                while (!channel.exhausted()) {
+                    val chunk = channel.readRemaining()
+                    count += chunk.remaining
+
+                    chunk.transferTo(stream)
+                    println("Received $count bytes from ${httpResponse.contentLength()}")
                 }
             }
-            println("A file saved to ${file.path}")
         }
+
+        println("A file saved to ${file.path}")
     }
 }
