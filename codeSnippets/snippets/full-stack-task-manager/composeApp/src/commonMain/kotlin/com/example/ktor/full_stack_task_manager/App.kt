3c29191a1499ad339acab171fc50ca95fa3e9071
Viktoriya Nikolova <contact@viknikolova.com>
2025-05-14 15:20:20 +0200

    include the "improve the UI" step in the full-stack-task-manager project

--- codeSnippets/snippets/full-stack-task-manager/composeApp/src/commonMain/kotlin/com/example/ktor/full_stack_task_manager/App.kt
+++ codeSnippets/snippets/full-stack-task-manager/composeApp/src/commonMain/kotlin/com/example/ktor/full_stack_task_manager/App.kt
@@ -1,16 +1,28 @@
 package com.example.ktor.full_stack_task_manager
 
+import Priority
 import Task
 import TaskApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ktor.full_stack_task_manager.network.createHttpClient
 import kotlinx.coroutines.launch
 
@@ -19,24 +31,62 @@ fun App() {
     MaterialTheme {
         val httpClient = createHttpClient()
         val taskApi = remember { TaskApi(httpClient) }
-        val tasks = remember { mutableStateOf(emptyList<Task>()) }
+        var tasks by remember { mutableStateOf(emptyList<Task>()) }
         val scope = rememberCoroutineScope()
 
-        Column(
-            modifier = Modifier
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = {
-                scope.launch {
-                    tasks.value = taskApi.getAllTasks()
-                }
-            }) {
-                Text("Fetch Tasks")
+        LaunchedEffect(Unit) {
+            tasks = taskApi.getAllTasks()
+        }
+
+        LazyColumn(modifier = Modifier.fillMaxSize().padding(8.dp)) {
+            items(tasks) { task ->
+                TaskCard(
+                    task,
+                    onDelete = {
+                        scope.launch {
+                            taskApi.removeTask(it)
+                            tasks = taskApi.getAllTasks()
+                        }
+                    },
+                    onUpdate = {
+                    }
+                )
             }
-            for (task in tasks.value) {
-                Text(task.name)
+        }
+    }
+}
+
+@Composable
+fun TaskCard(
+    task: Task,
+    onDelete: (Task) -> Unit,
+    onUpdate: (Task) -> Unit
+) {
+    fun pickWeight(priority: Priority) = when (priority) {
+        Priority.Low -> FontWeight.SemiBold
+        Priority.Medium -> FontWeight.Bold
+        Priority.High, Priority.Vital -> FontWeight.ExtraBold
+    }
+
+    Card(
+        modifier = Modifier.fillMaxWidth().padding(4.dp),
+        shape = RoundedCornerShape(CornerSize(4.dp))
+    ) {
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(
+                "${task.name}: ${task.description}",
+                fontSize = 20.sp,
+                fontWeight = pickWeight(task.priority)
+            )
+
+            Row {
+                OutlinedButton(onClick = { onDelete(task) }) {
+                    Text("Delete")
+                }
+                Spacer(Modifier.width(8.dp))
+                OutlinedButton(onClick = { onUpdate(task) }) {
+                    Text("Update")
+                }
             }
         }
     }
