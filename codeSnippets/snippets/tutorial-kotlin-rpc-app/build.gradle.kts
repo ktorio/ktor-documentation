ad0dc350a703023e92601229ea3574eef6f51ffe
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-07-18 11:06:31 +0200

    chore: Update kxrpc to 0.9.1 (#656)

--- codeSnippets/snippets/tutorial-kotlin-rpc-app/build.gradle.kts
+++ codeSnippets/snippets/tutorial-kotlin-rpc-app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.2.0"
     kotlin("plugin.serialization") version "2.2.0"
     id("io.ktor.plugin") version "3.2.0"
-    id("org.jetbrains.kotlinx.rpc.plugin") version "0.8.1"
+    id("org.jetbrains.kotlinx.rpc.plugin") version "0.9.1"
 }
 
 group = "com.example"
@@ -14,12 +14,12 @@ repositories {
 
 dependencies {
     implementation("io.ktor:ktor-client-cio-jvm")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-client:0.8.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-client:0.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-client:0.9.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-client:0.9.1")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-server:0.8.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-server:0.8.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-serialization-json:0.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-server:0.9.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-server:0.9.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-serialization-json:0.9.1")
     implementation("ch.qos.logback:logback-classic:1.5.18")
     testImplementation(kotlin("test"))
 }
