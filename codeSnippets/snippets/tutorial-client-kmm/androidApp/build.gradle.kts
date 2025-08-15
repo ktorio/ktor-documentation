320ec1e2aa2d0f9e3cd4010347f51bc0ed70a1e0
tokizuoh <tokizuoh@gmail.com>
2025-08-15 14:07:36 +0900

    fix: Remove redundant compose-compiler version configuration

--- codeSnippets/snippets/tutorial-client-kmm/androidApp/build.gradle.kts
+++ codeSnippets/snippets/tutorial-client-kmm/androidApp/build.gradle.kts
@@ -17,9 +17,6 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
