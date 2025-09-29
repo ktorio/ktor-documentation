c1221829cae5c9048ce3727bb0180bc9737d23ce
Viktoriya Nikolova <contact@viknikolova.com>
2025-09-29 17:13:07 +0200

    chore: feat: KTOR-7755 Add documentation for receiving multipart form data with the Ktor client

--- codeSnippets/snippets/_misc_client/ResponseTypes.kt
+++ codeSnippets/snippets/_misc_client/ResponseTypes.kt
@@ -15,3 +15,25 @@ val byteArrayBody: ByteArray = httpResponse.body()
 if (httpResponse.status.value in 200..299) {
     println("Successful response!")
 }
+
+// Multipart form data response
+val response = client.post("https://myserver.com/multipart/receive")
+
+val multipart = response.body<MultiPartData>()
+
+multipart.forEachPart { part ->
+    when (part) {
+        is PartData.FormItem -> {
+            println("Form item key: ${part.name}")
+            val value = part.value
+            // ...
+        }
+        is PartData.FileItem -> {
+            println("file: ${part.name}")
+            println(part.originalFileName)
+            val fileContent: ByteReadChannel = part.provider()
+            // ...
+        }
+    }
+    part.dispose()
+}
