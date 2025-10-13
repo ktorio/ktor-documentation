60ce3719d234b8711511bb241a6c6763ab212471
Vik Nikolova <contact@viknikolova.com>
2025-10-13 10:25:17 +0200

    KTOR-8556 Update the client KMP tutorial to use IntelliJ IDEA (#691)
    * KTOR-8556 Update the client KMM tutorial to use IntelliJ IDEA and update sample project
    
    * fix formatting, android code and screenshot
    
    * KTOR-8802 update iOS code
    
    * resolve comments
    
    * update AGP version to match generated code
    
    * address comments and update project name
    
    * add a note for android emulator and remove first step from ios chapter
    
    * update ktor and coroutines versions

---
+++ codeSnippets/snippets/tutorial-client-kmp/composeApp/src/androidUnitTest/kotlin/com/example/ktor/kmpktor/ComposeAppAndroidUnitTest.kt
@@ -0,0 +1,12 @@
+package com.example.ktor.kmpktor
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppAndroidUnitTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
