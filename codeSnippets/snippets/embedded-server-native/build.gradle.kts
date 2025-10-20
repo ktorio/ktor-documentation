babf9a0c47bb1ab37d3fd7301593bbae945f11b1
shub39 <cptnshubham39@gmail.com>
2025-10-20 16:50:44 +0530

    fix: KTOR-8988 native server setup for amd64 (#703)
    * KTOR-8988 fix native server setup for amd64
    
    * update client-engine-curl setup for amd64
    
    ---------
    
    Co-authored-by: vnikolova <contact@viknikolova.com>

--- codeSnippets/snippets/embedded-server-native/build.gradle.kts
+++ codeSnippets/snippets/embedded-server-native/build.gradle.kts
@@ -17,7 +17,7 @@ kotlin {
     val nativeTarget = when {
         hostOs == "Mac OS X" && arch == "x86_64" -> macosX64("native")
         hostOs == "Mac OS X" && arch == "aarch64" -> macosArm64("native")
-        hostOs == "Linux" && arch == "x86_64" -> linuxX64("native")
+        hostOs == "Linux" && (arch == "x86_64" || arch == "amd64") -> linuxX64("native")
         hostOs == "Linux" && arch == "aarch64" -> linuxArm64("native")
         hostOs.startsWith("Windows") -> mingwX64("native")
         // Other supported targets are listed here: https://ktor.io/docs/server-native.html#targets
