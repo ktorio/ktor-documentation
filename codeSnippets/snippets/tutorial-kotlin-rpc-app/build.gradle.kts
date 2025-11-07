405eae79290d444862b1574c0d0382c6dd8c22e0
vnikolova <contact@viknikolova.com>
2025-11-07 11:26:02 +0100

    bump Ktor version

--- codeSnippets/snippets/tutorial-kotlin-rpc-app/build.gradle.kts
+++ codeSnippets/snippets/tutorial-kotlin-rpc-app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.2.21"
     kotlin("plugin.serialization") version "2.2.21"
-    id("io.ktor.plugin") version "3.3.1"
+    id("io.ktor.plugin") version "3.3.2"
     id("org.jetbrains.kotlinx.rpc.plugin") version "0.10.1"
 }
 
@@ -14,12 +14,12 @@ repositories {
 
 dependencies {
     implementation("io.ktor:ktor-client-cio-jvm")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-client:0.10.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-client:0.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-client:0.10.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-client:0.10.0")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-server:0.10.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-server:0.10.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-serialization-json:0.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-server:0.10.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-server:0.10.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-serialization-json:0.10.0")
     implementation("ch.qos.logback:logback-classic:1.5.18")
     testImplementation(kotlin("test"))
 }
