d65dc5c4233131acb4bd88f9b3d5ebdcf282af9f
vnikolova <contact@viknikolova.com>
2026-08-20 13:47:15 +0200

    docs: feat: KTOR-9791 add a what's new entry and example for onCallValidators

---
+++ codeSnippets/snippets/custom-plugin/src/main/kotlin/com/example/plugins/UserValidationPlugin.kt
@@ -0,0 +1,13 @@
+package com.example.plugins
+
+import io.ktor.server.application.*
+
+val UserValidationPlugin = createRouteScopedPlugin("UserValidationPlugin") {
+    onCallValidators {
+        call -> val principal = call.principal<UserIdPrincipal>()
+
+        if (principal != null) {
+            application.log.info("Validating request for ${principal.name}")
+        }
+    }
+}
